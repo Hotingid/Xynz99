cc481d27b97e78b2b939d9c28695caf78e76dfd5
Hostingid <xynz.99hosting@gmail.com>
2022-10-06 18:52:11 +0700

    Xynz99

---
+++ config.js
@@ -0,0 +1,56 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev
+   * 
+   * Contact Me on wa.me/6282113821188
+   * Follow https://github.com/nazedev
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6281212236166']
+global.ownernomer = "6281212236166"
+global.premium = ['6281212236166']
+global.packname = 'STICKER BY'
+global.author = 'BJORTAK'
+global.sessionName = 'data'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Berhasil ✅',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'KHUSUS OWNER BANG BUKAN OWNER? SKIP🗿',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 45
+}
+global.thumb = fs.readFileSync('./lib/naze.png')
+global.vaze = { url: 'https://telegra.ph/file/750b2f6aaa1472e14cfca.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
